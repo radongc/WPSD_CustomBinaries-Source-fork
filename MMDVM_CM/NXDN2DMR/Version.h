f8ac5861ef5f1672729056997478a97537c773b9
Chipster <go@w.ay>
2025-09-01 05:02:22 -0500

    update mmdvm oled code to wpsd version

--- MMDVM_CM/NXDN2DMR/Version.h
+++ MMDVM_CM/NXDN2DMR/Version.h
@@ -20,7 +20,7 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20250831_WPSD";
+const char* VERSION = "20250901_WPSD";
 
 #endif
 
--- MMDVM_CM/USRP2P25/Version.h
+++ MMDVM_CM/USRP2P25/Version.h
@@ -20,6 +20,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20250831_WPSD";
+const char* VERSION = "20250901_WPSD";
 
 #endif

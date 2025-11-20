06ed50973ccebd2c422b45b748428ed57eb8fc3a
Chipster <go@w.ay>
2025-11-20 04:40:42 -0600

    fixed typo

--- APRSGateway/Version.h
+++ APRSGateway/Version.h
@@ -19,6 +19,6 @@
 #if !defined(VERSION_H)
 #define	VERSION_H
 
-const char* VERSION = "20250924_WPSD";
+const char* VERSION = "20251120_WPSD";
 
 #endif
--- ircDDBGateway/Common/Version.h
+++ ircDDBGateway/Common/Version.h
@@ -24,9 +24,9 @@
 const wxString VENDOR_NAME = wxT("G4KLX");
 
 #if defined(__WXDEBUG__)
-const wxString VERSION = wxT("20250924_WPSD");
+const wxString VERSION = wxT("20251120_WPSD");
 #else
-const wxString VERSION = wxT("20250924_WPSD");
+const wxString VERSION = wxT("20251120_WPSD");
 #endif
 
 #endif
